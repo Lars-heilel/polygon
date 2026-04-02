82f5b2cf531ac3be07c01b05adc05f85fa1b2977
Lars Heilel <heuchler228@gmail.com>
2026-04-03 02:18:40 +0300

    chore: add jest coverage thresholds (10% initial, to be raised iteratively)

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,13 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  coverageThreshold: {
+    global: {
+      lines: 10,
+      branches: 10,
+      functions: 10,
+      statements: 10,
+    },
+  },
+};
